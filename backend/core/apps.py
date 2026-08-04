d181e427989a3c87f49a7cee39ee17b2af62f7f7
Alep <65488656+Korialstrasz2@users.noreply.github.com>
2026-08-04 21:23:35 +0200

    Install theme reveal extension

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -5,3 +5,8 @@ class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend.core"
     verbose_name = "Nucleo ReDjango"
+
+    def ready(self) -> None:
+        from .theme_reveal_patch import install
+
+        install()
